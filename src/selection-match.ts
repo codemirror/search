979520c1e7ef43dfb4a55607797e8a0bfa4fbb03
Alf Eaton <eaton.alf@gmail.com>
2022-02-25 09:52:33 +0000

    Remove check for falsy query

--- src/selection-match.ts
+++ src/selection-match.ts
@@ -86,7 +86,6 @@ const matchHighlighter = ViewPlugin.fromClass(class {
       if (len < conf.minSelectionLength || len > 200) return Decoration.none
       if (conf.wholeWords) {
         query = state.sliceDoc(range.from, range.to) // TODO: allow and include leading/trailing space?
-        if (!query) return Decoration.none
         check = state.charCategorizer(range.head)
         if (!(insideWordBoundaries(check, state, range.from, range.to)
             && insideWord(check, state, range.from, range.to))) return Decoration.none
