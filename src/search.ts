a78d42bd9c8d1f552f42ab9cf10f7374a6b048bc
Marijn Haverbeke <marijn@haverbeke.nl>
2022-11-12 17:50:00 +0100

    Don't hide close button in search dialog for read-only editor
    FIX: Fix a bug that hid the search dialog's close button when the editor was
    read-only.
    
    Closes https://github.com/codemirror/dev/issues/997

--- src/search.ts
+++ src/search.ts
@@ -628,14 +628,14 @@ class SearchPanel implements Panel {
         elt("br"),
         this.replaceField,
         button("replace", () => replaceNext(view), [phrase(view, "replace")]),
-        button("replaceAll", () => replaceAll(view), [phrase(view, "replace all")]),
-        elt("button", {
-          name: "close",
-          onclick: () => closeSearchPanel(view),
-          "aria-label": phrase(view, "close"),
-          type: "button"
-        }, ["×"])
-      ]
+        button("replaceAll", () => replaceAll(view), [phrase(view, "replace all")])
+      ],
+      elt("button", {
+        name: "close",
+        onclick: () => closeSearchPanel(view),
+        "aria-label": phrase(view, "close"),
+        type: "button"
+      }, ["×"])
     ])
   }
 
